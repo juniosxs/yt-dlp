17a30f73cfac02901f7a67f081d95c99b99c5676
Junio <juniox1997@gmail.com>
2026-05-05 10:59:49 -0300

    update

---
+++ download-all.bat
@@ -0,0 +1,6 @@
+@echo off
+yt-dlp.exe --config-location "config\edm-remix.conf"
+yt-dlp.exe --config-location "config\gospel variado.conf"
+yt-dlp.exe --config-location "config\pop.conf"
+yt-dlp.exe --config-location "config\rock.conf"
+pause
